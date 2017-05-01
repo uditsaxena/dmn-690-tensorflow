6e392dff271805e03c57e57e9a614ba01052b743
Udit Saxena <saxena.udit@gmail.com>
2017-04-30 21:57:17 -0400

    Changing for slurm scheduler.

--- slurm_basic/11.sh
+++ slurm_basic/11.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --job-name=dmnb_11
 #SBATCH --mem=10000
-
+#SBATCH --partition=titanx-long
 #SBATCH --output=us_experiment_%A.out
 #SBATCH --error=us_experiment_%A.err
 
@@ -10,8 +10,14 @@
 echo $SLURM_JOBID - `hostname` >> ~/slurm-lstm-jobs.txt
 
 module purge
-module load tensorflow/1.0.0
+
 module load python/3.5.2
+module load cuda75/blas/7.5.18
+module load cuda75/fft/7.5.18
+module load cuda75/nccl/7.5.18
+module load cuda75/nsight/7.5.18
+module load cuda75/profiler/7.5.18
+module load cuda75/toolkit/7.5.18
 cd /home/usaxena/work/690/dmn
 python main.py --task 11 --test false
 python main.py --task 11 --test true
